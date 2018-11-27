02c1709985b617cac8206e524f1c1a833eaffdfe
Priydarshi Singh <dryairship@gmail.com>
2018-11-27 15:48:25 +0530

    Created the program file.

---
+++ simewo.py
@@ -0,0 +1,36 @@
+import sys
+import re
+
+def find(word):
+	# pattern contains the regular expression used to match words in the file
+	pattern = '(^'+word
+
+	for i in range(0, len(word)+1):
+		# this line considers the word formed by removing one character from the original word
+		pattern += '$|^'+word[0:i]+word[i+1:len(word)]
+
+		# this loop considers words formed by adding each character from A to Z, one by one, in the original word
+		for j in range(65,91):
+			pattern += '$|^'+word[0:i]+chr(j)+word[i:len(word)]
+	
+	pattern += '$)'
+
+	# this array contains the list of words that matched with the regex pattern
+	list = []
+
+	# matching the regex pattern with the file
+	with open('sowpods.txt') as file:
+		list = re.findall(pattern,file.read(),re.MULTILINE)
+
+	
+	print 'Similar Meaningful Words : '
+	for simewo in list:
+		print simewo
+
+if __name__ == '__main__':
+	if(len(sys.argv)!=2):
+		print "Invalid syntax."
+		print "Usage : simewo.py <word>"
+	else:
+		# find is called only if exactly one word is provided as input
+		find(sys.argv[1].upper())
